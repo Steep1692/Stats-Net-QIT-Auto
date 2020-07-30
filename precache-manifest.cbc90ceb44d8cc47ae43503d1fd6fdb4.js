172d484294c9e2ab283c9953b0fe4e3d1e08e760
user <1111>
2020-07-30 08:08:29 +0300

    Updates

---
+++ precache-manifest.cbc90ceb44d8cc47ae43503d1fd6fdb4.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49abf6ebdceb95e64f3915df6b80c2e4",
+    "url": "/Stats-Net-QIT-Auto/index.html"
+  },
+  {
+    "revision": "b74a989b18feb728f61d",
+    "url": "/Stats-Net-QIT-Auto/static/css/main.ca9cce08.chunk.css"
+  },
+  {
+    "revision": "fc137e102cf27884245a",
+    "url": "/Stats-Net-QIT-Auto/static/js/2.7849eb23.chunk.js"
+  },
+  {
+    "revision": "a31666e30466bd9de17f6868b1be69ad",
+    "url": "/Stats-Net-QIT-Auto/static/js/2.7849eb23.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b74a989b18feb728f61d",
+    "url": "/Stats-Net-QIT-Auto/static/js/main.5f025db8.chunk.js"
+  },
+  {
+    "revision": "a1b7ddd652a1c4d652ccf0b254dba9df",
+    "url": "/Stats-Net-QIT-Auto/static/js/main.5f025db8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f874229a2ff5b448c91",
+    "url": "/Stats-Net-QIT-Auto/static/js/runtime-main.beeb6b90.js"
+  },
+  {
+    "revision": "1dc1a4f23a7442c068b6605317ef2689",
+    "url": "/Stats-Net-QIT-Auto/static/media/adventage2.1dc1a4f2.svg"
+  },
+  {
+    "revision": "d05f1f4aac9a58a0e302541f0b3c272c",
+    "url": "/Stats-Net-QIT-Auto/static/media/adventage3.d05f1f4a.svg"
+  },
+  {
+    "revision": "291dde53fd70db936a63b2726f1100b1",
+    "url": "/Stats-Net-QIT-Auto/static/media/adventage4.291dde53.svg"
+  },
+  {
+    "revision": "f9fea046bcc8fea1802a7988297b5a00",
+    "url": "/Stats-Net-QIT-Auto/static/media/arrow-b.f9fea046.svg"
+  },
+  {
+    "revision": "eaab6ca9168202ff48a8d88f2be030d8",
+    "url": "/Stats-Net-QIT-Auto/static/media/arrow-down.eaab6ca9.svg"
+  },
+  {
+    "revision": "bec0dfa510ae5f290422c3d156f8d3dd",
+    "url": "/Stats-Net-QIT-Auto/static/media/card.bec0dfa5.svg"
+  },
+  {
+    "revision": "ff12597f278fa17263c3040599376efb",
+    "url": "/Stats-Net-QIT-Auto/static/media/close.ff12597f.svg"
+  },
+  {
+    "revision": "eaeb4d21ff7077e95f30928b8f091569",
+    "url": "/Stats-Net-QIT-Auto/static/media/info.eaeb4d21.svg"
+  },
+  {
+    "revision": "28d4fc3e0d467e16f6ef6105493c33ad",
+    "url": "/Stats-Net-QIT-Auto/static/media/mob.28d4fc3e.svg"
+  },
+  {
+    "revision": "b94858661183b3f5da19ce069d6b5940",
+    "url": "/Stats-Net-QIT-Auto/static/media/photo.b9485866.svg"
+  },
+  {
+    "revision": "ace2ae8192e86ffccf6886027850059e",
+    "url": "/Stats-Net-QIT-Auto/static/media/plus.ace2ae81.svg"
+  },
+  {
+    "revision": "099a95eb7adafbcaf057f54944aa6161",
+    "url": "/Stats-Net-QIT-Auto/static/media/visa.099a95eb.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.498d0d70d35bec7ee5717c518b433282.js"
+  "/Stats-Net-QIT-Auto/precache-manifest.cbc90ceb44d8cc47ae43503d1fd6fdb4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Stats-Net-QIT-Auto/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
